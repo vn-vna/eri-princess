fe4ef0bae532a698f4ecdbad386c4492a058ed02
Anh Vũ <vn.vna@outlook.com>
2022-01-06 19:52:29 +0700

    Initialize Code

---
+++ srcs/index.js
@@ -0,0 +1,22 @@
+const { Client, Intents } = require('discord.js');
+const DotEnv = require("dotenv")
+
+DotEnv.config()
+
+const token = process.env.BOT_TOKEN;
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.on("message", (message) => {
+    console.log("Message has been created")
+    if (message.content === "ds#ping")
+    {
+        message.reply("pong")
+    }
+})
+
+client.login(token);
