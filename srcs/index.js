81bf077ba3f7d27bfc6a4e73b25441d1ab98a712
Anh Vũ <vn.vna@outlook.com>
2022-01-08 21:11:35 +0700

    Exlicit DEV mode and PRODUCTION mode

--- srcs/index.js
+++ srcs/index.js
@@ -1,11 +1,13 @@
 const { Client, Intents } = require('discord.js')
 const DotEnv = require("dotenv")
+const FileSystem = require("fs")
 const commandEval = require("./msg-command/command-eval");
 const messageWatcher = require("./msg-watcher/message-watcher")
 
-DotEnv.config()
+if (FileSystem.existsSync(`${__dirname}/../.env`)) DotEnv.config()
 
 const token = process.env.BOT_TOKEN
+const ERI_COMMAND = process.env.ENVIRONMENT == "DEV" ? "_eri" : "eri"
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
 
@@ -14,7 +16,7 @@ client.once('ready', () => {
 })
 
 client.on("messageCreate", (message) => {
-    if (message.content.startsWith("eri")) {
+    if (message.content.startsWith(ERI_COMMAND)) {
         commandEval(message.content.split(" "), message)
     } else {
         messageWatcher(message)
